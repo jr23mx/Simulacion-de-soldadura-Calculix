28836176491216120f6cbcff52cca71cdd40dea0
Jean Abraham <jeanabrahamchc@gmail.com>
2026-05-25 16:53:25 -0600

    code fixes

--- setup/descompress-and-compile-ccx-pardiso.sh
+++ setup/descompress-and-compile-ccx-pardiso.sh
@@ -16,7 +16,7 @@ echo "Parche: $CUSTOM_PATCHES_DIR"
 read -p "¿Quieres aplicar este parche? [s/n] " resp
 if [[ "$resp" == "s" ]]; then
     # rm -R "$PATCHES_DIR" # Esto esta por trolling. En realidad no hace falta borrar nadota, solo copiar.
-    cp -R "$CUSTOM_PATCHES_DIR" "$PATCHES_DIR"
+    cp -R "$CUSTOM_PATCHES_DIR/"* "$PATCHES_DIR/"
 else
     echo "Nimodo..."
 fi
@@ -25,10 +25,10 @@ echo
 
 echo "# Compilando"
 BUILD_SCRIPT="$SCRIPT_DIR/$CCX_PARDISO_NAME/ccx/build.sh"
-chmod 777 $BUILD_SCRIPT
-$BUILD_SCRIPT
+chmod 777 "$BUILD_SCRIPT"
+"$BUILD_SCRIPT"
 echo; echo
 
 echo "# Mostrando archivo. Si se compilo, deben estar los exes"
 INSTALL_DIR="$SCRIPT_DIR/$CCX_PARDISO_NAME/ccx/x64/install"
-ls $INSTALL_DIR
+ls "$INSTALL_DIR"
